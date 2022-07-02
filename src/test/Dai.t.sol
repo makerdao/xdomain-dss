a319cac9cf43f1eca518c23df4cddc0a4788e50a
Sam MacPherson <xylom44@gmail.com>
2022-07-02 11:15:37 -0400

    fix license (completely new code)

--- src/test/Dai.t.sol
+++ src/test/Dai.t.sol
@@ -1,22 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-/// erc20.t.sol -- test for erc20.sol
-
-// Copyright (C) 2015-2019  DappHub, LLC
-
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 pragma solidity ^0.8.13;
 
 import "dss-test/DSSTest.sol";
