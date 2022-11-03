6398c8c360ca946460da95d4d2fbad690990eec3
Sam MacPherson <xylom44@gmail.com>
2022-11-03 13:54:05 -0400

    check for rely

--- src/test/ClaimToken.t.sol
+++ src/test/ClaimToken.t.sol
@@ -48,6 +48,8 @@ contract ClaimTokenTest is DSSTest {
         keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
 
     function postSetup() internal virtual override {
+        vm.expectEmit(true, true, true, true);
+        emit Rely(address(this));
         token = new ClaimToken();
     }
 
